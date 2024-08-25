abda84690cac48d4024ac198014915e40c634a6c
trastanechora <maestro.trastanechora@tokopedia.com>
2024-08-25 13:35:07 +0700

    feat(backend): add patient entity

---
+++ src/backend/entities/patient/types.ts
@@ -0,0 +1,46 @@
+import { IdentifierUse } from '../shared/types';
+import type { Telecomunication } from '../telecomunication/types';
+
+/**
+ * @description a set of codes that can be used to indicate a type of gender used for administrative purposes
+ * @source https://hl7.org/fhir/codesystem-administrative-gender.html
+ */
+export enum GenderType {
+  Male = 'male',
+  Female = 'female',
+  Other = 'other',
+  Unknown = 'unknown',
+}
+
+/**
+ * @description a set of codes that can be used to identifies the purpose for this identifier, if known
+ * @source https://hl7.org/fhir/codesystem-identifier-use.html
+ */
+type PatientIdentifierUse = IdentifierUse;
+
+interface PatientIdentifier {
+  system: string;
+  use: PatientIdentifierUse;
+  value: string;
+}
+
+interface Name {
+  use: string;
+  text: string;
+  family: string;
+  given: string;
+  prefix: string;
+  suffix: string;
+  period: string;
+}
+
+export interface Patient {
+  active: boolean;
+  telecomunications: Telecomunication[];
+  gender: GenderType;
+  identifier: PatientIdentifier;
+  birthDate: string; // YYYY-MM-DD
+  deceased: string;
+  deceasedBoolean: boolean;
+  deceasedDateTIme: string;
+}
